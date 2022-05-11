c8bb159ff40f873ce3b4a3e7baf11f87cd80d35e
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-05-12 00:45:50 +0600

    tabs

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Tabs from './components/Tabs.vue'
+import Tab from './components/Tab.vue'
+</script>
+
+<template>
+  <Tabs>
+    <Tab title='Tab One'>Lorem ipsum dolor sit, amet. 1</Tab>
+    <Tab title='Tab Two'>Lorem ipsum dolor sit, amet. 2</Tab>
+    <Tab title='Tab Three'>Lorem ipsum dolor sit, amet. 3</Tab>
+    <Tab title='Tab Four'>Lorem ipsum dolor sit, amet. 4</Tab>
+    <Tab title='Tab Five'>Lorem ipsum dolor sit, amet. 5</Tab>
+  </Tabs>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
